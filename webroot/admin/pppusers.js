33eb8f760d92a29bf62dcd0f6ed2170d4f148ee2
James Baicoianu <james_github@baicoianu.com>
2024-11-25 08:48:59 +0000

    Fixed syntax error

--- webroot/admin/pppusers.js
+++ webroot/admin/pppusers.js
@@ -220,7 +220,7 @@ class PPPUserGraph extends HTMLElement {
         .append("text")
           .attr("x", width * .9 + size*1.2)
           .attr("y", function(d,i){ return 10 + i*(size+5) + (size/2)}) // 100 is where the first dot appears. 25 is the distance between dots
-          .attr("class", d => 'label ' + this.sanitizeSelector(d) + (this.banlist.indexOf(d) != -1 ? ' banned' : '');)
+          .attr("class", d => 'label ' + this.sanitizeSelector(d) + (this.banlist.indexOf(d) != -1 ? ' banned' : ''))
           .style("fill", function(d){ return color(d)})
           .text(function(d){ return d})
           .attr("text-anchor", "left")
